7053049020aeeb0f5a31d471f239dd947a5c7d83
Lam1902 <chutunglam1902@gmail.com>
2024-07-17 22:59:59 +0700

    cập nhật lần 4 17/7/2024

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,8 @@
 // khai báo các domain đc chấp nhận
 export const WHITELIST_DOMAINS = [
     'http://localhost:5173',
-    undefined
+    undefined,
+    'https://web-trello-lamjs.vercel.app'
 ]
 
 export const BOARD_TYPES = {
